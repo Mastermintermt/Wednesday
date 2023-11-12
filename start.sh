767ad99b8bd43df199f76d132e7d7f5e5e5422ce
AGSMOD <149931558+AGSMOD@users.noreply.github.com>
2023-11-12 13:05:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/AGSMOD/Alto.git /Alto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /Alto
 fi
-cd /PiroAutoFilterBot
+cd /Alto
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting Alto...."
 python3 bot.py
