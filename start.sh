81ceb7136a5df66d8a9c1d00ef2d8a8fadaaaee6
Mastermintermt <150274518+Mastermintermt@users.noreply.github.com>
2023-12-01 21:33:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AGSMOD/Wednesdayv3.git /Alto
+  git clone https://github.com/Mastermintermt/Wednesday.git /Alto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Wednesdayv3
 fi
 cd /Alto
 pip3 install -U -r requirements.txt
-echo "Starting Wednesdayv3...."
+echo "Starting Wednesday...."
 python3 bot.py
