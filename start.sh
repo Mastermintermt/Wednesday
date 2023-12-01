8e56667063c63f923d1240db4d8b469765dbcb72
AGSMOD <149931558+AGSMOD@users.noreply.github.com>
2023-12-01 20:56:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AGSMOD/Alto.git /Alto
+  git clone https://github.com/AGSMOD/Wednesdayv3.git /Alto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Alto
+  git clone $UPSTREAM_REPO /Wednesdayv3
 fi
 cd /Alto
 pip3 install -U -r requirements.txt
-echo "Starting Alto...."
+echo "Starting Wednesdayv3...."
 python3 bot.py
